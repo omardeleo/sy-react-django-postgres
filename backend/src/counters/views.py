51a4656485026976d0dac447e42f8812667fb39d
Omar De Leo <omar.deleo@gmail.com>
2021-01-06 14:45:08 -0500

    Update Django response

--- backend/src/counters/views.py
+++ backend/src/counters/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+import datetime
 
 from .models import Counter
 
@@ -11,5 +12,7 @@ def index(request):
     else:
         counter = Counter()
         counter.save()
-    response = f'Hi! I\'m a Django server. Been pinged {counter.value} times.'
+    date = datetime.datetime.now()
+    dateStr = date.strftime('%c')
+    response = f'Hi! I\'m a Django server.\n\nI\'m running on port 8080.\nI\'ve been pinged {counter.value} times.\nMost recent ping on {dateStr}.'
     return JsonResponse({"response": response})
